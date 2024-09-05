d595c8de2eeb299066794902c2379b171a989306
BOUJBAIR OUSSAMAE <93010609+Boujbair@users.noreply.github.com>
2024-09-05 13:28:06 +0100

    add changes

--- app.py
+++ app.py
@@ -9,10 +9,11 @@ from resources.user import blp as UserBlueprint
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
-
+from dotenv import load_dotenv
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
